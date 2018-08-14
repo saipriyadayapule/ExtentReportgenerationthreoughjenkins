8b964d133a482374239e56844ff380fc304e874f
Shinu Suresh <mailtoshinu@gmail.com>
2018-08-14 12:00:22 +0100

    extentreport generation through jenkins

---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+
+	
+	import org.testng.annotations.Test;
+
+	public class LoginPageTest {
+
+		@Test
+		public void startloginpage() {
+			System.out.println("Loginpagetest");
+		}
+	}
+		
